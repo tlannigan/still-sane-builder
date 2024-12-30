c3322b2ac7051310e28df597cbd50bf03b74aa6c
tlannigan <tristanlannigan@gmail.com>
2024-12-29 23:03:18 -0700

    Align pane header with pane content

--- src/components/ui/panes/PaneHeader.tsx
+++ src/components/ui/panes/PaneHeader.tsx
@@ -7,7 +7,7 @@ const PaneHeader = ({
   title: string
   side: 'left' | 'right'
 }) => (
-  <div className="relative shrink-0 flex flex-col justify-end items-center h-[7.5rem] font-thin bg-darkest p-2">
+  <div className="relative shrink-0 flex flex-col justify-end items-center h-[7.5rem] font-thin bg-darkest py-2 pr-4">
     <div className="w-[10rem] desktop-1080:w-[15rem] desktop-1440:w-[20rem] h-10 bg-amber-300 content-center text-center text-black transition-[width]">
       <h2 className="text-2xl">{title}</h2>
       <CloseButton side={side} className="absolute right-8 bottom-3" />
