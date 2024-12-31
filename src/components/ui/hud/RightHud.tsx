88c819de6497debc0edbd6885e56cdc3fd712834
tlannigan <tristanlannigan@gmail.com>
2024-12-31 10:25:20 -0700

    Fix padding height in right HUD

--- src/components/ui/hud/RightHud.tsx
+++ src/components/ui/hud/RightHud.tsx
@@ -6,7 +6,7 @@ const RightHud = () => (
   <div className="w-[21.5rem] desktop-1080:w-[32.25rem] desktop-1440:w-[43rem] h-[6.25rem] desktop-1080:h-[9.375rem] desktop-1440:h-[12.5rem] rounded-tl-[3rem] desktop-1080:rounded-tl-[4.5rem] desktop-1440:rounded-tl-[6rem] bg-background">
     <ManaGlobe />
 
-    <div className="w-[11.6rem] desktop-1080:w-[17.4rem] desktop-1440:w-[23.2rem] h-full flex flex-col gap-y-[2rem] justify-center pl-[0.76875rem] desktop-1080:pl-[1.153125rem] desktop-1440:pl-[1.5375rem] pt-[3rem] desktop-1080:pt-[4.5rem] desktop-1440:pt-[0.6rem]">
+    <div className="w-[11.6rem] desktop-1080:w-[17.4rem] desktop-1440:w-[23.2rem] h-full flex flex-col gap-y-[2rem] justify-center pl-[0.76875rem] desktop-1080:pl-[1.153125rem] desktop-1440:pl-[1.5375rem] pt-[0.3rem] desktop-1080:pt-[0.45rem] desktop-1440:pt-[0.6rem]">
       <div className="flex justify-between items-center">
         <div className="grow flex justify-center mt-[0.1rem] desktop-1080:mt-[0.15rem] desktop-1440:mt-[0.2rem]">
           <TownPortalButton />
