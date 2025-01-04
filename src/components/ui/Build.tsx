7c762f66f173a4e4de29b2d4ddecae48d7a28c00
tlannigan <tristanlannigan@gmail.com>
2025-01-04 00:34:09 -0700

    Wrap builds in listings in Link tags

--- src/components/ui/Build.tsx
+++ src/components/ui/Build.tsx
@@ -1,30 +1,32 @@
 import Link from 'next/link'
 
 const Build = () => (
-  <div className="flex items-center gap-x-4 bg-light pl-16 py-4">
-    <Link href="/build/123">
+  <Link href="/build/123" className="group">
+    <div className="flex items-center gap-x-4 bg-light pl-16 py-4">
       <div className="w-[4.5rem] h-[4.5rem] bg-black" />
-    </Link>
 
-    <div className="h-[4.5rem] flex flex-col justify-between">
-      <div>
-        <Link href="/build/123" className="hover:text-amber-400">
-          <h3 className="font-thin">Archmage Spark</h3>
-        </Link>
-        <p className="text-xs">By CaptainLance9</p>
-      </div>
+      <div className="h-[4.5rem] flex flex-col justify-between">
+        <div>
+          <h3 className="font-thin group-hover:text-amber-400">
+            Archmage Spark
+          </h3>
+          <p className="text-xs">By CaptainLance9</p>
+        </div>
 
-      <div className="flex gap-x-2">
-        <p className="text-white text-xs bg-darker px-2 py-1 rounded">
-          Starter
-        </p>
-        <p className="text-white text-xs bg-darker px-2 py-1 rounded">
-          Bossing
-        </p>
-        <p className="text-white text-xs bg-darker px-2 py-1 rounded">Cheap</p>
+        <div className="flex gap-x-2">
+          <p className="text-white text-xs bg-darker px-2 py-1 rounded">
+            Starter
+          </p>
+          <p className="text-white text-xs bg-darker px-2 py-1 rounded">
+            Bossing
+          </p>
+          <p className="text-white text-xs bg-darker px-2 py-1 rounded">
+            Cheap
+          </p>
+        </div>
       </div>
     </div>
-  </div>
+  </Link>
 )
 
 export default Build
